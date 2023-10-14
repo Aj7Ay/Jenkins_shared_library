34e60c4de6b4d6637f14e2c8b2fc20effb132320
Aj7ay <ajayyegireddi007@gmail.com>
2023-10-14 17:13:03 +0530

    updated

--- vars/owaspDependencyCheck.groovy
+++
@@ -1,8 +0,0 @@
-def call() {
-  // Get OWASP Dependency Check tool
-  def odc = tool 'DP-Check'
-  // Run scan
-  sh "${odc}/bin/dependency-check.sh --scan ."
-  // Publish results
-  dependencyCheck additionalArguments: '', aggregatingResults: true, archive: true, healthy: '', pattern: '**/dependency-check-report.xml', unHealthy: ''
-}
