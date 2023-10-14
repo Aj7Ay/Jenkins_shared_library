b011d279956a0a7d521d4871d268d4e7d8f06760
Aj7ay <ajayyegireddi007@gmail.com>
2023-10-14 20:32:30 +0530

    kube files added

---
+++ vars/kubeDeploy.groovy
@@ -0,0 +1,5 @@
+def call() {
+    withKubeConfig(caCertificate: '', clusterName: '', contextName: '', credentialsId: 'k8s', namespace: '', restrictKubeConfigAccess: false, serverUrl: '') {
+        sh "kubectl apply -f deployment.yml"
+    }
+}
