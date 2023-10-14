c0abadaf1c61ea08527a3ec62cdf1ed20d0ee4ee
Aj7ay <ajayyegireddi007@gmail.com>
2023-10-14 20:49:01 +0530

    updated Deletek8s

--- vars/kubeDelete.groovy
+++ vars/kubeDelete.groovy
@@ -1,5 +1,5 @@
 def call() {
     withKubeConfig(caCertificate: '', clusterName: '', contextName: '', credentialsId: 'k8s', namespace: '', restrictKubeConfigAccess: false, serverUrl: '') {
-        sh "kubectl delete deployment myapp-deployment"
+        sh "kubectl delete -f deployment.yml"
     }
 }
