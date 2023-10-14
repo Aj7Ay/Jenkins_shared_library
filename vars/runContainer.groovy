24baa97d660cd49c5b1f42499070f1910315a40e
Aj7ay <ajayyegireddi007@gmail.com>
2023-10-14 20:06:32 +0530

    updated Runconatiner

--- vars/runContainer.groovy
+++ vars/runContainer.groovy
@@ -1,3 +1,3 @@
-def call(String containerName, String ports, String imageName){
-    sh "docker run -d --name ${containerName} -p ${ports} ${imageName}"
+def call(){
+    sh "docker run -d --name youtube1 -p 3000:3000 sevenajay/youtube:latest"
 }
