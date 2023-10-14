dedcb020ce0b6bdc93a085adbbacc81a41df77e7
Aj7ay <ajayyegireddi007@gmail.com>
2023-10-14 19:19:01 +0530

    updated docker

--- vars/dockerBuild.groovy
+++ vars/dockerBuild.groovy
@@ -1,6 +1,10 @@
-def call() {
-    sh '''
-    docker build --build-arg REACT_APP_RAPID_API_KEY=f0ead79813mshb0aa7ddf114a7dap1adb3djsn483b017de1a9 -t youtube .
-    docker tag youtube sevenajay/youtube:latest
-    '''
+def call(String dockerHubUsername, String imageName) {
+    // Build the Docker image
+    sh "docker build --build-arg REACT_APP_RAPID_API_KEY=f0ead79813mshb0aa7ddf114a7dap1adb3djsn483b017de1a9 -t ${imageName} ."
+     // Tag the Docker image
+    sh "docker tag ${imageName} ${dockerHubUsername}/${imageName}:latest"
+    // Push the Docker image
+    withDockerRegistry([url: 'https://index.docker.io/v1/', credentialsId: 'docker']) {
+        sh "docker push ${dockerHubUsername}/${imageName}:latest"
+    }
 }
