76de52560c75b7e3740c83713b137cf4fbcedc92
Aj7ay <ajayyegireddi007@gmail.com>
2023-10-14 16:01:20 +0530

    updated sonarqube Analysis file

--- vars/sonarqubeAnalysis.groovy
+++ vars/sonarqubeAnalysis.groovy
@@ -1,11 +1,5 @@
-// vars/sonarqubeAnalysis.groovy
-
 def call() {
-    stage('SonarQube Analysis') {
-        steps {
-            withSonarQubeEnv('sonar-server') {
-                sh ''' $SCANNER_HOME/bin/sonar-scanner -Dsonar.projectName=Netflix -Dsonar.projectKey=Netflix '''
-            }
-        }
+    withSonarQubeEnv('sonar-server') {
+        sh ''' $SCANNER_HOME/bin/sonar-scanner -Dsonar.projectName=Youtube -Dsonar.projectKey=Youtube '''
     }
 }
